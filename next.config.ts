572d4472c03299f5770248a1d5282f0fb9312602
Greg Newkirk <gregnewkirk@gmail.com>
2026-04-02 12:18:07 -0700

    Add /reel redirect to YouTube demo reel

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async redirects() {
+    return [
+      {
+        source: "/reel",
+        destination: "https://youtu.be/KMZWRu7mBEs",
+        permanent: false,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
