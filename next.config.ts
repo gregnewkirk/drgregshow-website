765b3aa2779cae4eaf23738c4a334bcb2c3f71a6
Greg Newkirk <gregnewkirk@gmail.com>
2026-06-17 13:07:06 -0700

    Redirect donate subdomain to Stripe

--- next.config.ts
+++ next.config.ts
@@ -12,6 +12,17 @@ const nextConfig: NextConfig = {
   async redirects() {
     return [
       {
+        source: "/:path*",
+        has: [
+          {
+            type: "host",
+            value: "donate.drgregshow.com",
+          },
+        ],
+        destination: "https://buy.stripe.com/7sYeVd0CWcwp0Vb4Hu6Ri01",
+        permanent: false,
+      },
+      {
         source: "/reel",
         destination: "https://youtu.be/KMZWRu7mBEs",
         permanent: false,
